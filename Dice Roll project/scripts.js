86c17c7595e7d6fa0659167671b068a165ac7b5f
Varun <varunpawardigital@gmail.com>
2024-03-18 21:59:25 +0530

    completed the base functionality , edge cases left!

--- Dice Roll project/scripts.js
+++ Dice Roll project/scripts.js
@@ -6,6 +6,8 @@ const player0CurrentScore = document.querySelector('.player-0-current-score');
 const player1CurrentScore = document.querySelector('.player-1-current-score');
 const diceImage = document.querySelector('.dice-img');
 const rollDiceBtn = document.querySelector('.roll-dice-btn');
+const holdBtn = document.querySelector('.hold-score-btn');
+const resetBtn = document.querySelector('.new-game-btn');
 
 // things to do when game starts
 player0TotalScore.textContent = 0;
@@ -45,4 +47,77 @@ rollDiceBtn.addEventListener('click', function () {
       .querySelector('.player-1')
       .classList.toggle('active-player-indicator-color');
   }
+});
+
+// now when user clicks on hold
+holdBtn.addEventListener('click', function () {
+  // 1. add the current score to the total score
+  document.querySelector(`.player-${activePlayer}-total-score`).textContent =
+    currentScore;
+
+  // 2. set the current score of user to zero.
+
+  currentScore = 0;
+
+  document.querySelector(
+    `.player-${activePlayer}-current-score`
+  ).textContent = 0;
+
+  // 3. if (user has more than 100pts in their total score then they win , otherwise player swtiches)
+  if (
+    document.querySelector(`.player-${activePlayer}-total-score`).textContent >=
+    25
+  ) {
+    //  user wins
+    console.log('you win motherfucker');
+
+    document
+      .querySelector(`.player-${activePlayer}`)
+      .classList.add('player-winner');
+
+    document.querySelector(`.player-${activePlayer}-label`).style.color =
+      '#c7365f';
+  } else {
+    // player switches
+    // switch to next player
+    document.querySelector(
+      `.player-${activePlayer}-current-score`
+    ).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+
+    document
+      .querySelector('.player-0')
+      .classList.toggle('active-player-indicator-color');
+    document
+      .querySelector('.player-1')
+      .classList.toggle('active-player-indicator-color');
+  }
+});
+
+// when user resets the game
+
+resetBtn.addEventListener('click', function () {
+  document
+    .querySelector(`.player-${activePlayer}`)
+    .classList.remove('player-winner');
+
+  document.querySelector(`.player-${activePlayer}-label`).style.color = '#333';
+
+  diceImage.classList.add('hidden');
+
+  player0CurrentScore.textContent = 0;
+  player1CurrentScore.textContent = 0;
+  player0TotalScore.textContent = 0;
+  player1TotalScore.textContent = 0;
+
+  currentScore = 0;
+  activePlayer = 0;
+
+  document
+    .querySelector('.player-0')
+    .classList.add('active-player-indicator-color');
+  document
+    .querySelector('.player-1')
+    .classList.remove('active-player-indicator-color');
 });
